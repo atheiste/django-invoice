7bf1e50499bf12f13eae09634b99c6556fd9ec4e
Tomas Peterka <prestizni@gmail.com>
2016-10-30 00:15:32 +0200

    bump v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-invoice2",
-    version="0.1",
+    version="0.1.1",
     description='Pluggable django invoicing app',
     packages=[
         'invoice',
